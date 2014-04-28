2b608f7ca3ba385cab12c2e8ad79fd56c6477676
manasij7479 <manasij7479@gmail.com>
2014-04-28 17:53:15 +0530

    ctags wrapper

--- include/cling/CtagsExtension/Wrapper.h
+++ include/cling/CtagsExtension/Wrapper.h
@@ -1,6 +1,34 @@
 #ifndef CLING_CTAGS_WRAPPER_H
 #define CLING_CTAGS_WRAPPER_H
+
+
+#include "readtags.h"
+#include<vector>
+#include<cstdlib>
+#include<map>
+#include<string>
+
 namespace cling {
-  
+  class TagFileWrapper{
+  public:
+    TagFileWrapper(std::string path);
+    TagFileWrapper(const std::vector<std::string>& file_list);
+    
+    struct LookupResult
+    {
+        std::string name;
+        std::string kind;
+    };
+    
+    //returns a map that maps headers to lookup results 
+    std::map<std::string,LookupResult> match(std::string name, bool partialMatch=false);
+private:
+    void generate(const std::vector<std::string>& cmd,int arglimit=50);
+    bool read();
+    tagFile* tf;
+    tagFileInfo tfi;
+    std::string tagfilename;
+      
+  };
 }
 #endif
---
+++ lib/CtagsExtension/Wrapper.cpp
@@ -0,0 +1,106 @@
+#include "cling/CtagsExtension/Wrapper.h"
+#include <ftw.h>
+#include <fnmatch.h>
+#include <unistd.h>
+namespace cling {
+  TagFileWrapper::TagFileWrapper(const std::vector<std::string>& file_list)
+  {
+      generate(file_list);
+      read();
+  }
+  
+  static const char *headertypes[] = 
+  {
+     "*.hh", "*.hpp", "*.h"
+  };
+  static std::vector<std::string> FilePaths; ///TODO:Find a way to do this without globals
+  static const int maxfd=512;
+  
+  static int callback(const char *fpath, const struct stat *sb, int typeflag)
+  {
+    /* if it's a file */
+    if (typeflag == FTW_F)
+    {
+      int i;
+      /* for each filter, */
+      for (i = 0; i < sizeof(headertypes) / sizeof(headertypes[0]); i++) 
+      {
+        /* if the filename matches the filter, */
+        if (fnmatch(headertypes[i], fpath, FNM_CASEFOLD) == 0) 
+        {
+          /* do something */
+          FilePaths.push_back(fpath);
+          break;
+        }
+      }
+    }
+    return 0;
+  }
+  
+  TagFileWrapper::TagFileWrapper(std::string path)
+  {
+    ftw(path.c_str(),callback,maxfd);
+    generate(FilePaths);
+    read();
+  }
+  
+  
+  
+  std::map<std::string,TagFileWrapper::LookupResult> 
+  TagFileWrapper::match(std::string name, bool partialMatch)
+  {
+    std::map<std::string,LookupResult> map;
+    tagEntry entry;
+    int options=TAG_OBSERVECASE | (partialMatch?TAG_PARTIALMATCH:TAG_FULLMATCH);
+    
+    tagResult result = tagsFind(tf, &entry, name.c_str(),options);
+    
+    while(result==TagSuccess)
+    {
+      LookupResult r;
+      r.name=entry.name;
+      r.kind=entry.kind;
+      map[entry.file]=r;
+      result=tagsFindNext(tf,&entry);
+    }
+    
+    return map;
+  }
+  //no more than `arglimit` arguments in a single invocation
+  void TagFileWrapper::generate(const std::vector<std::string>& paths,int arglimit)
+  {
+    auto it=paths.begin();
+    int no_of_args=0;
+    std::string concat;
+    tagfilename="tags";
+    std::remove(tagfilename.c_str());
+    while(it!=paths.end())
+    {
+      concat+=(*it+" ");
+      no_of_args++;
+      if(no_of_args==arglimit)
+      {
+        std::string filename=" -f "+tagfilename+" ";
+        std::string sorted=" --sort=yes ";
+        std::string append=" -a ";
+        std::string cmd="ctags "+append+filename+sorted+concat;
+        std::system(cmd.c_str());
+        
+        no_of_args=0;
+        it++;
+      }
+    }
+  }
+  
+  bool TagFileWrapper::read()
+  {
+    tf=tagsOpen(tagfilename.c_str(),&tfi);
+    if(tfi.status.opened == false)
+    {
+        //throw a std::runtime_error ?
+//          std::cerr<<"Failed to open tag file.\n";
+        return false;
+    }
+    return true;
+  }
+}

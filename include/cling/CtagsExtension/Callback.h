f2bd7577f92a134a42ab48fc9fc8fbc7cafdd246
manasij7479 <manasij7479@gmail.com>
2014-04-30 19:53:46 +0530

    got rid of -Woverloaded-virtual warning

--- include/cling/CtagsExtension/Callback.h
+++ include/cling/CtagsExtension/Callback.h
@@ -3,12 +3,9 @@
 
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/Interpreter/InterpreterCallbacks.h"
-#include "cling/CtagsExtension/Wrapper.h"
 #include "cling/CtagsExtension/TagManager.h"
 #include "clang/Sema/Lookup.h"
-#include <string>
-#include <map>
-#include <vector>
+
 namespace cling{
   class LookupInfo{};//TODO: Would contain info regarding previous lookups; get rid of the map in LookupObject
   
@@ -16,14 +13,15 @@ namespace cling{
   public:
       CtagsInterpreterCallback(cling::Interpreter* interp,cling::TagManager* t);
     
+    using cling::InterpreterCallbacks::LookupObject;
+      //^to get rid of bogus warning : "-Woverloaded-virtual"
+      //virtual functions ARE meant to be overriden!
+
     bool LookupObject (clang::LookupResult &R, clang::Scope *);
     
-    //void AddTagFile(llvm::StringRef path);
     cling::TagManager* getTagManager();
   private:
-    //std::map<std::string,LookupInfo> table;
     cling::Interpreter* ip;
-    //std::vector<TagFileWrapper> tags;
     cling::TagManager* tags;
   };
 }
--- lib/CtagsExtension/Callback.cpp
+++ lib/CtagsExtension/Callback.cpp
@@ -2,23 +2,8 @@
 
 namespace cling {
   bool CtagsInterpreterCallback::LookupObject (clang::LookupResult &R, clang::Scope *){
-    std::string in=R.getLookupName().getAsString();
-//    if(in[0]!='_' && table.find(in)==table.end())
-//    {
-////       llvm::outs()<<"Missing name:"<<in<<"\n";
-////      for(auto& tagfile:tags)
-////      {
-////        for(auto result:tagfile.match(in,true))
-////        {
-////          llvm::outs()<<result.first
-////            <<'\t'<<result.second.name
-////            <<'\t'<<result.second.kind
-////            <<'\n';
-////        }
-////      }
-//      table[in]=LookupInfo();
 
-//    }
+    std::string in=R.getLookupName().getAsString();
     for(auto it=tags->begin(in);it!=tags->end(in);++it)
     {
         auto lookup=it->second;
@@ -29,26 +14,14 @@ namespace cling {
     }
     return false;
   }
-//  void CtagsInterpreterCallback::AddTagFile(llvm::StringRef path) {
-//    //llvm::outs()<<"Tagfile generated from: "<<path<<"\n";
-//    TagFileWrapper tf(path);
-//    if(!tf.emptyFile())
-//        tags.push_back(tf);
-//  }
+
 
   CtagsInterpreterCallback::CtagsInterpreterCallback
   (cling::Interpreter* interp, TagManager *t) :
     InterpreterCallbacks(interp,true),
     ip(interp),
     tags(t) {
-//      llvm::SmallVector<std::string,10> paths;
-//      interp->GetIncludePaths(paths,true,false);
-//      for(auto p:paths)
-//      {
-//          //llvm::outs()<<p<<"\n";
-//          //Takes too long
-//      }
-      //AddTagFile("/usr/include/c++/");
+
 
   }
   TagManager* CtagsInterpreterCallback::getTagManager()

e28f36635d0c58df50fa93725f62fc89079cbfeb
manasij7479 <manasij7479@gmail.com>
2014-05-20 06:32:54 +0530

    added brief doxygen docs for TagManager and TagFileWrapper

--- include/cling/TagsExtension/TagManager.h
+++ include/cling/TagsExtension/TagManager.h
@@ -4,11 +4,14 @@
 #include <map>
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/TagsExtension/Wrapper.h"
+
 namespace cling {
+  ///\brief Class for managing all the available tag files
   class TagManager {
   public:
     TagManager();
     
+    ///\brief Information from a tag file lookup
     class LookupInfo{
     public:
       LookupInfo(std::string h,std::string n,std::string t);
@@ -16,15 +19,18 @@ namespace cling {
       std::string name;
       std::string type;
     };
-    
+    ///\brief Add a new path from which a single tag file is generated
+    /// If recurse is true, the path is recursively scanned but not preprocessed
+    /// else, only the files directly in path are preprocessed and tagged
     void AddTagFile(llvm::StringRef path,bool recurse=true);
+
+
     typedef std::multimap<std::string,LookupInfo> TableType;
     TableType::iterator begin(std::string name);
     TableType::iterator end(std::string name);
 
   private:
     std::vector<TagFileWrapper*> m_tags;
-    //cling::Interpreter* ip;
     TableType m_table;
     
   };

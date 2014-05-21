9d5e8f504d756d22761424c0bf26fca81e779311
manasij7479 <manasij7479@gmail.com>
2014-05-22 03:20:57 +0530

    basic test case, more to follow

---
+++ test/TagsExtension/LookupTest.C
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+// RUN: cat %s | %cling 2>&1
+// Test LookupTest
+
+.rawInput 0
+#include "cling/TagsExtension/TagManager.h"
+#include "cling/TagsExtension/Callback.h"
+cling::TagManager t;
+gCling->setCallbacks(new cling::AutoloadCallback(gCling,&t));
+
+.T %S
+
+Foo f;//expected-warning {{Note: 'Foo' can be found in TestHeader.h}} 
+.q
+

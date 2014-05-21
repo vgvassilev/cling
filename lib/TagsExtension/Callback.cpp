b3958554dcf5664f92a23378a26cc43cbd5a5da6
manasij7479 <manasij7479@gmail.com>
2014-05-21 10:58:37 +0530

    proper disgnostics instead of llvm::outs()

--- lib/TagsExtension/Callback.cpp
+++ lib/TagsExtension/Callback.cpp
@@ -1,17 +1,29 @@
 #include "cling/TagsExtension/Callback.h"
+#include "clang/Basic/Diagnostic.h"
 #include "llvm/ADT/SmallVector.h"
+#include "clang/Sema/Sema.h"
+#include "llvm/Support/Path.h"
 
 namespace cling {
+
   bool AutoloadCallback::LookupObject (clang::LookupResult &R, clang::Scope *){
     std::string in=R.getLookupName().getAsString();
+    const char fmt[]="Note: '%0' can be found in %1";
+    const char note[]="Type : %0 , Full Path: %1";
+    auto id=m_ip->getSema().getDiagnostics().getCustomDiagID(clang::DiagnosticsEngine::Level::Warning,fmt);
+    auto idn=m_ip->getSema().getDiagnostics().getCustomDiagID(clang::DiagnosticsEngine::Level::Note,note);
     for(auto it=m_tags->begin(in);it!=m_tags->end(in);++it)
     {
       auto lookup=it->second;
-      llvm::outs() << lookup.header
-                  << '\t' << lookup.name
-                  << '\t' <<lookup.type
-                  << '\n';
+//      llvm::outs() << lookup.header
+//                  << '\t' << lookup.name
+//                  << '\t' <<lookup.type
+//                  << '\n';
+      m_ip->getSema().Diags.Report(id) << lookup.name << llvm::sys::path::filename(lookup.header);
+      m_ip->getSema().Diags.Report(idn) << lookup.type << lookup.header;
+
     }
+
     return false;
   }
 

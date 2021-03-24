6eedbe285f9941ff1843507f0b220b5abf5d7c46
Pratyush Das <reikdas@gmail.com>
2021-03-24 19:44:46 +0530

    isystem should read patch header first

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -616,10 +616,9 @@ def compile_for_binary(arg):
 
     patch_path = os.path.join(CLING_SRC_DIR, "patches", "llvm90-headers")
     build = Build()
-    cmake_config_flags = (clangdir + ' -DCMAKE_BUILD_TYPE={0} -DCMAKE_INSTALL_PREFIX={1} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON '
-                          .format(build.buildType, TMP_PREFIX) + llvm_flags +
+    cmake_config_flags = (clangdir + ' -DCMAKE_CXX_FLAGS="-isystem {2}" -DCMAKE_C_FLAGS="-isystem {2}" -DCMAKE_BUILD_TYPE={0} -DCMAKE_INSTALL_PREFIX={1} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON '
+                          .format(build.buildType, TMP_PREFIX, patch_path) + llvm_flags +
                           ' -DLLVM_TARGETS_TO_BUILD=host;NVPTX -DCLING_CXX_HEADERS=ON -DCLING_INCLUDE_TESTS=ON' +
-                          ' -DCMAKE_CXX_FLAGS="-isystem {0}" -DCMAKE_C_FLAGS="-isystem {0}"'.format(patch_path) +
                           EXTRA_CMAKE_FLAGS)
     box_draw('Configure Cling with CMake ' + cmake_config_flags)
     exec_subprocess_call('%s %s' % (CMAKE, cmake_config_flags), LLVM_OBJ_ROOT, True)

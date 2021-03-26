8ee51215484617de851576529d2f42e1717943c8
Pratyush Das <reikdas@gmail.com>
2021-03-26 15:38:18 +0530

    Try without install_prefix

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -2199,7 +2199,7 @@ if args['current_dev']:
             else:
                 compile(os.path.join(workdir,
                                      'cling-' + DIST + '-' + REV + '-' + platform.machine().lower() + '-' + VERSION))
-        install_prefix()
+        #install_prefix()
         if not args['no_test']:
             if args['with_binary_llvm']:
                 setup_tests()

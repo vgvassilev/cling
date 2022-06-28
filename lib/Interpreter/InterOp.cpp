27930d6930342a93b909753e90f5daf9574607ec
Vassil Vassilev <v.g.vassilev@gmail.com>
2022-06-28 17:51:09 +0000

    Move code in a separate file now part of clingInterpreter.

---
+++ lib/Interpreter/InterOp.cpp
@@ -0,0 +1,113 @@
+//--------------------------------------------------------------------*- C++ -*-
+// CLING - the C++ LLVM-based InterpreterG :)
+// author:  Vassil Vassilev <vvasilev@cern.ch>
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+#include "cling/Interpreter/InterOp.h"
+
+#include "clang/AST/Decl.h"
+#include "clang/AST/DeclCXX.h"
+#include "clang/AST/RecordLayout.h"
+#include "clang/Sema/Sema.h"
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Casting.h"
+
+namespace cling {
+namespace InterOp {
+  using namespace clang;
+  using namespace llvm;
+
+  bool IsNamespace(TCppScope_t scope) {
+    Decl *D = static_cast<Decl*>(scope);
+    return isa<NamespaceDecl>(D);
+  }
+  // See TClingClassInfo::IsLoaded
+  bool IsComplete(TCppScope_t scope) {
+    if (!scope)
+      return false;
+    Decl *D = static_cast<Decl*>(scope);
+    if (auto *CXXRD = dyn_cast<CXXRecordDecl>(D))
+      return CXXRD->hasDefinition();
+    else if (auto *TD = dyn_cast<TagDecl>(D))
+      return TD->getDefinition();
+
+    // Everything else is considered complete.
+    return true;
+  }
+
+  size_t SizeOf(TCppScope_t scope) {
+    assert (scope);
+    if (!IsComplete(scope))
+      return 0;
+
+    if (auto *RD = dyn_cast<RecordDecl>(static_cast<Decl*>(scope))) {
+      ASTContext &Context = RD->getASTContext();
+      const ASTRecordLayout &Layout = Context.getASTRecordLayout(RD);
+      return Layout.getSize().getQuantity();
+    }
+
+    return 0;
+  }
+
+  bool IsBuiltin(TCppType_t type) {
+    QualType Ty = QualType::getFromOpaquePtr(type);
+    if (Ty->isBuiltinType() || Ty->isAnyComplexType())
+      return true;
+    // FIXME: Figure out how to avoid the string comparison.
+    return llvm::StringRef(Ty.getAsString()).contains("complex");
+  }
+
+  bool IsTemplate(TCppScope_t handle) {
+    auto *D = (clang::Decl *)handle;
+    return llvm::isa_and_nonnull<clang::TemplateDecl>(D);
+  }
+
+  bool IsAbstract(TCppType_t klass) {
+    auto *D = (clang::Decl *)klass;
+    if (auto *CXXRD = llvm::dyn_cast_or_null<clang::CXXRecordDecl>(D))
+      return CXXRD->isAbstract();
+
+    return false;
+  }
+
+  bool IsEnum(TCppScope_t handle) {
+    auto *D = (clang::Decl *)handle;
+    return llvm::isa_and_nonnull<clang::EnumDecl>(D);
+  }
+
+  bool IsVariable(TCppScope_t scope) {
+    auto *D = (clang::Decl *)scope;
+    return llvm::isa_and_nonnull<clang::VarDecl>(D);
+  }
+
+  std::string GetName(TCppType_t klass) {
+    // In cppyy GlobalScope is represented by empty string
+    // if (klass == Cppyy::NewGetGlobalScope())
+    //     return "";
+
+    auto *D = (clang::NamedDecl *) klass;
+    return D->getNameAsString();
+  }
+
+  std::vector<TCppScope_t> GetUsingNamespaces(TCppScope_t scope) {
+    auto *D = (clang::Decl *) scope;
+
+    if (auto *DC = llvm::dyn_cast_or_null<clang::DeclContext>(D)) {
+      std::vector<TCppScope_t> namespaces;
+      for (auto UD : DC->using_directives()) {
+        namespaces.push_back((TCppScope_t) UD->getNominatedNamespace());
+      }
+      return namespaces;
+    }
+
+    return {};
+  }
+} // end namespace InterOp
+
+} // end namespace cling
+
--- unittests/libInterOp/ScopeReflectionTest.cpp
+++ unittests/libInterOp/ScopeReflectionTest.cpp
@@ -1,118 +1,24 @@
 
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/Interpreter/Transaction.h"
+#include "cling/Interpreter/InterOp.h"
 
+#include "clang/Config/config.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclCXX.h"
-#include "clang/AST/RecordLayout.h"
 #include "clang/Basic/Version.h"
-#include "clang/Config/config.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Sema/Sema.h"
 
 #include "llvm/ADT/StringRef.h"
-#include "llvm/Support/Casting.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Path.h"
 
 #include "gtest/gtest.h"
 
+using namespace cling;
 using namespace clang;
 using namespace llvm;
-namespace libInterOp {
-using TCppScope_t = void*;
-using TCppType_t = void*;
-
-bool IsNamespace(TCppScope_t scope) {
-  Decl *D = static_cast<Decl*>(scope);
-  return isa<NamespaceDecl>(D);
-}
-// See TClingClassInfo::IsLoaded
-bool IsComplete(TCppScope_t scope) {
-  if (!scope)
-    return false;
-  Decl *D = static_cast<Decl*>(scope);
-  if (auto *CXXRD = dyn_cast<CXXRecordDecl>(D))
-    return CXXRD->hasDefinition();
-  else if (auto *TD = dyn_cast<TagDecl>(D))
-    return TD->getDefinition();
-
-  // Everything else is considered complete.
-  return true;
-}
-
-size_t SizeOf(TCppScope_t scope) {
-  assert (scope);
-  if (!IsComplete(scope))
-    return 0;
-
-  if (auto *RD = dyn_cast<RecordDecl>(static_cast<Decl*>(scope))) {
-    ASTContext &Context = RD->getASTContext();
-    const ASTRecordLayout &Layout = Context.getASTRecordLayout(RD);
-    return Layout.getSize().getQuantity();
-  }
-
-  return 0;
-}
-
-bool IsBuiltin(TCppType_t type) {
-  QualType Ty = QualType::getFromOpaquePtr(type);
-  if (Ty->isBuiltinType() || Ty->isAnyComplexType())
-    return true;
-  // FIXME: Figure out how to avoid the string comparison.
-  return llvm::StringRef(Ty.getAsString()).contains("complex");
-}
-
-bool IsTemplate(TCppScope_t handle)
-{
-    auto *D = (clang::Decl *)handle;
-    return llvm::isa_and_nonnull<clang::TemplateDecl>(D);
-}
-
-bool IsAbstract(TCppType_t klass)
-{
-    auto *D = (clang::Decl *)klass;
-    if (auto *CXXRD = llvm::dyn_cast_or_null<clang::CXXRecordDecl>(D))
-      return CXXRD->isAbstract();
-
-    return false;
-}
-
-bool IsEnum(TCppScope_t handle)
-{
-    auto *D = (clang::Decl *)handle;
-    return llvm::isa_and_nonnull<clang::EnumDecl>(D);
-}
-
-bool IsVariable(TCppScope_t scope)
-{
-    auto *D = (clang::Decl *)scope;
-    return llvm::isa_and_nonnull<clang::VarDecl>(D);
-}
-
-std::string GetName(TCppType_t klass)
-{
-    // In cppyy GlobalScope is represented by empty string
-    // if (klass == Cppyy::NewGetGlobalScope())
-    //     return "";
-
-    auto *D = (clang::NamedDecl *) klass;
-    return D->getNameAsString();
-}
-
-std::vector<TCppScope_t> GetUsingNamespaces(TCppScope_t scope)
-{
-    auto *D = (clang::Decl *) scope;
-
-    if (auto *DC = llvm::dyn_cast_or_null<clang::DeclContext>(D)) {
-        std::vector<TCppScope_t> namespaces;
-        for (auto UD : DC->using_directives()) {
-            namespaces.push_back((TCppScope_t) UD->getNominatedNamespace());
-        }
-        return namespaces;
-    }
-
-    return {};
-}
-}
 
 // This function isn't referenced outside its translation unit, but it
 // can't use the "static" keyword because its address is used for
@@ -143,23 +49,23 @@ static std::string MakeResourcesPath() {
   return std::string(P.str());
 }
 
-static std::unique_ptr<cling::Interpreter> createInterpreter() {
+static std::unique_ptr<Interpreter> createInterpreter() {
   std::string MainExecutableName =
     llvm::sys::fs::getMainExecutable(nullptr, nullptr);
   std::string ResourceDir = MakeResourcesPath();
   std::vector<const char *> ClingArgv = {"-resource-dir", ResourceDir.c_str()};
   ClingArgv.insert(ClingArgv.begin(), MainExecutableName.c_str());
-  return llvm::make_unique<cling::Interpreter>(ClingArgv.size(), &ClingArgv[0]);
+  return llvm::make_unique<Interpreter>(ClingArgv.size(), &ClingArgv[0]);
 }
 
-std::unique_ptr<cling::Interpreter> Interp;
+std::unique_ptr<Interpreter> Interp;
 
 static void GetAllTopLevelDecls(const std::string& code, std::vector<Decl*>& Decls) {
   Interp = createInterpreter();
-  cling::Transaction *T = nullptr;
+  Transaction *T = nullptr;
   Interp->declare(code, &T);
   for (auto DCI = T->decls_begin(), E = T->decls_end(); DCI != E; ++DCI) {
-    if (DCI->m_Call != cling::Transaction::kCCIHandleTopLevelDecl)
+    if (DCI->m_Call != Transaction::kCCIHandleTopLevelDecl)
       continue;
     assert(DCI->m_DGR.isSingleDecl());
     Decls.push_back(DCI->m_DGR.getSingleDecl());
@@ -177,21 +83,21 @@ static void GetAllSubDecls(Decl *D, std::vector<Decl*>& SubDecls) {
 TEST(ScopeReflectionTest, IsNamespace) {
   std::vector<Decl*> Decls;
   GetAllTopLevelDecls("namespace N {} class C{}; int I;", Decls);
-  EXPECT_TRUE(libInterOp::IsNamespace(Decls[0]));
-  EXPECT_FALSE(libInterOp::IsNamespace(Decls[1]));
-  EXPECT_FALSE(libInterOp::IsNamespace(Decls[2]));
+  EXPECT_TRUE(InterOp::IsNamespace(Decls[0]));
+  EXPECT_FALSE(InterOp::IsNamespace(Decls[1]));
+  EXPECT_FALSE(InterOp::IsNamespace(Decls[2]));
 }
 
 TEST(ScopeReflectionTest, IsComplete) {
   std::vector<Decl*> Decls;
   GetAllTopLevelDecls("namespace N {} class C{}; int I; struct S; enum E : int; union U{};",
                       Decls);
-  EXPECT_TRUE(libInterOp::IsComplete(Decls[0]));
-  EXPECT_TRUE(libInterOp::IsComplete(Decls[1]));
-  EXPECT_TRUE(libInterOp::IsComplete(Decls[2]));
-  EXPECT_FALSE(libInterOp::IsComplete(Decls[3]));
-  EXPECT_FALSE(libInterOp::IsComplete(Decls[4]));
-  EXPECT_TRUE(libInterOp::IsComplete(Decls[5]));
+  EXPECT_TRUE(InterOp::IsComplete(Decls[0]));
+  EXPECT_TRUE(InterOp::IsComplete(Decls[1]));
+  EXPECT_TRUE(InterOp::IsComplete(Decls[2]));
+  EXPECT_FALSE(InterOp::IsComplete(Decls[3]));
+  EXPECT_FALSE(InterOp::IsComplete(Decls[4]));
+  EXPECT_TRUE(InterOp::IsComplete(Decls[5]));
 }
 
 TEST(ScopeReflectionTest, SizeOf) {
@@ -201,14 +107,14 @@ TEST(ScopeReflectionTest, SizeOf) {
                         struct Size16 {short a; double b;};
                        )";
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[0]), (size_t)0);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[1]), (size_t)1);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[2]), (size_t)0);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[3]), (size_t)0);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[4]), (size_t)0);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[5]), (size_t)1);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[6]), (size_t)4);
-  EXPECT_EQ(libInterOp::SizeOf(Decls[7]), (size_t)16);
+  EXPECT_EQ(InterOp::SizeOf(Decls[0]), (size_t)0);
+  EXPECT_EQ(InterOp::SizeOf(Decls[1]), (size_t)1);
+  EXPECT_EQ(InterOp::SizeOf(Decls[2]), (size_t)0);
+  EXPECT_EQ(InterOp::SizeOf(Decls[3]), (size_t)0);
+  EXPECT_EQ(InterOp::SizeOf(Decls[4]), (size_t)0);
+  EXPECT_EQ(InterOp::SizeOf(Decls[5]), (size_t)1);
+  EXPECT_EQ(InterOp::SizeOf(Decls[6]), (size_t)4);
+  EXPECT_EQ(InterOp::SizeOf(Decls[7]), (size_t)16);
 }
 
 TEST(ScopeReflectionTest, IsBuiltin) {
@@ -218,17 +124,17 @@ TEST(ScopeReflectionTest, IsBuiltin) {
   //  "float", "double", "long double", "void"}
 
   ASTContext &C = Interp->getCI()->getASTContext();
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.BoolTy.getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.CharTy.getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.SignedCharTy.getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.VoidTy.getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.BoolTy.getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.CharTy.getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.SignedCharTy.getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.VoidTy.getAsOpaquePtr()));
   // ...
 
   // complex
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.getComplexType(C.FloatTy).getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.getComplexType(C.DoubleTy).getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.getComplexType(C.LongDoubleTy).getAsOpaquePtr()));
-  EXPECT_TRUE(libInterOp::IsBuiltin(C.getComplexType(C.Float128Ty).getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.getComplexType(C.FloatTy).getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.getComplexType(C.DoubleTy).getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.getComplexType(C.LongDoubleTy).getAsOpaquePtr()));
+  EXPECT_TRUE(InterOp::IsBuiltin(C.getComplexType(C.Float128Ty).getAsOpaquePtr()));
 
   // std::complex
   std::vector<Decl*> Decls;
@@ -237,7 +143,7 @@ TEST(ScopeReflectionTest, IsBuiltin) {
   auto lookup = S.getStdNamespace()->lookup(&C.Idents.get("complex"));
   auto *CTD = cast<ClassTemplateDecl>(lookup.front());
   for (ClassTemplateSpecializationDecl *CTSD : CTD->specializations())
-    EXPECT_TRUE(libInterOp::IsBuiltin(C.getTypeDeclType(CTSD).getAsOpaquePtr()));
+    EXPECT_TRUE(InterOp::IsBuiltin(C.getTypeDeclType(CTSD).getAsOpaquePtr()));
 }
 
 TEST(ScopeReflectionTest, IsTemplate) {
@@ -260,10 +166,10 @@ TEST(ScopeReflectionTest, IsTemplate) {
                         void g() {} )";
 
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_TRUE(libInterOp::IsTemplate(Decls[0]));
-  EXPECT_FALSE(libInterOp::IsTemplate(Decls[1]));
-  EXPECT_TRUE(libInterOp::IsTemplate(Decls[2]));
-  EXPECT_FALSE(libInterOp::IsTemplate(Decls[3]));
+  EXPECT_TRUE(InterOp::IsTemplate(Decls[0]));
+  EXPECT_FALSE(InterOp::IsTemplate(Decls[1]));
+  EXPECT_TRUE(InterOp::IsTemplate(Decls[2]));
+  EXPECT_FALSE(InterOp::IsTemplate(Decls[3]));
 }
 
 TEST(ScopeReflectionTest, IsAbstract) {
@@ -277,8 +183,8 @@ TEST(ScopeReflectionTest, IsAbstract) {
   )";
 
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_FALSE(libInterOp::IsAbstract(Decls[0]));
-  EXPECT_TRUE(libInterOp::IsAbstract(Decls[1]));
+  EXPECT_FALSE(InterOp::IsAbstract(Decls[0]));
+  EXPECT_TRUE(InterOp::IsAbstract(Decls[1]));
 }
 
 TEST(ScopeReflectionTest, IsEnum) {
@@ -295,9 +201,9 @@ TEST(ScopeReflectionTest, IsEnum) {
   )";
 
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_TRUE(libInterOp::IsEnum(Decls[0]));
-  EXPECT_FALSE(libInterOp::IsEnum(Decls[1]));
-  EXPECT_FALSE(libInterOp::IsEnum(Decls[2]));
+  EXPECT_TRUE(InterOp::IsEnum(Decls[0]));
+  EXPECT_FALSE(InterOp::IsEnum(Decls[1]));
+  EXPECT_FALSE(InterOp::IsEnum(Decls[2]));
 }
 
 TEST(ScopeReflectionTest, IsVariable) {
@@ -313,15 +219,15 @@ TEST(ScopeReflectionTest, IsVariable) {
   )";
 
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_TRUE(libInterOp::IsVariable(Decls[0]));
-  EXPECT_FALSE(libInterOp::IsVariable(Decls[1]));
+  EXPECT_TRUE(InterOp::IsVariable(Decls[0]));
+  EXPECT_FALSE(InterOp::IsVariable(Decls[1]));
 
   std::vector<Decl *> SubDecls;
   GetAllSubDecls(Decls[1], SubDecls);
-  EXPECT_FALSE(libInterOp::IsVariable(SubDecls[0]));
-  EXPECT_FALSE(libInterOp::IsVariable(SubDecls[1]));
-  EXPECT_FALSE(libInterOp::IsVariable(SubDecls[2]));
-  EXPECT_TRUE(libInterOp::IsVariable(SubDecls[3]));
+  EXPECT_FALSE(InterOp::IsVariable(SubDecls[0]));
+  EXPECT_FALSE(InterOp::IsVariable(SubDecls[1]));
+  EXPECT_FALSE(InterOp::IsVariable(SubDecls[2]));
+  EXPECT_TRUE(InterOp::IsVariable(SubDecls[3]));
 }
 
 TEST(ScopeReflectionTest, GetName) {
@@ -331,14 +237,14 @@ TEST(ScopeReflectionTest, GetName) {
                         struct Size16 {short a; double b;};
                        )";
   GetAllTopLevelDecls(code, Decls);
-  EXPECT_EQ(libInterOp::GetName(Decls[0]), "N");
-  EXPECT_EQ(libInterOp::GetName(Decls[1]), "C");
-  EXPECT_EQ(libInterOp::GetName(Decls[2]), "I");
-  EXPECT_EQ(libInterOp::GetName(Decls[3]), "S");
-  EXPECT_EQ(libInterOp::GetName(Decls[4]), "E");
-  EXPECT_EQ(libInterOp::GetName(Decls[5]), "U");
-  EXPECT_EQ(libInterOp::GetName(Decls[6]), "Size4");
-  EXPECT_EQ(libInterOp::GetName(Decls[7]), "Size16");
+  EXPECT_EQ(InterOp::GetName(Decls[0]), "N");
+  EXPECT_EQ(InterOp::GetName(Decls[1]), "C");
+  EXPECT_EQ(InterOp::GetName(Decls[2]), "I");
+  EXPECT_EQ(InterOp::GetName(Decls[3]), "S");
+  EXPECT_EQ(InterOp::GetName(Decls[4]), "E");
+  EXPECT_EQ(InterOp::GetName(Decls[5]), "U");
+  EXPECT_EQ(InterOp::GetName(Decls[6]), "Size4");
+  EXPECT_EQ(InterOp::GetName(Decls[7]), "Size16");
 }
 
 TEST(ScopeReflectionTest, GetUsingNamespaces) {
@@ -357,10 +263,10 @@ TEST(ScopeReflectionTest, GetUsingNamespaces) {
 
   GetAllTopLevelDecls(code, Decls);
   std::vector<void *> usingNamespaces;
-  usingNamespaces = libInterOp::GetUsingNamespaces(
+  usingNamespaces = InterOp::GetUsingNamespaces(
           Decls[0]->getASTContext().getTranslationUnitDecl());
 
-  EXPECT_EQ(libInterOp::GetName(usingNamespaces[0]), "runtime");
-  EXPECT_EQ(libInterOp::GetName(usingNamespaces[1]), "std");
-  EXPECT_EQ(libInterOp::GetName(usingNamespaces[2]), "abc");
+  EXPECT_EQ(InterOp::GetName(usingNamespaces[0]), "runtime");
+  EXPECT_EQ(InterOp::GetName(usingNamespaces[1]), "std");
+  EXPECT_EQ(InterOp::GetName(usingNamespaces[2]), "abc");
 }

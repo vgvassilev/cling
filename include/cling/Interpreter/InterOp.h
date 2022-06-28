148211f5ae655a18a0366b3a11c3055704edb48a
Vassil Vassilev <v.g.vassilev@gmail.com>
2022-06-28 17:51:09 +0000

    Move code in a separate file now part of clingInterpreter.

---
+++ include/cling/Interpreter/InterOp.h
@@ -0,0 +1,43 @@
+//--------------------------------------------------------------------*- C++ -*-
+// CLING - the C++ LLVM-based InterpreterG :)
+// author:  Vassil Vassilev <vvasilev@cern.ch>
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+#ifndef CLING_INTERPRETER_INTEROP_H
+#define CLING_INTERPRETER_INTEROP_H
+
+#include <string>
+
+namespace cling {
+namespace InterOp {
+  using TCppScope_t = void*;
+  using TCppType_t = void*;
+
+  bool IsNamespace(TCppScope_t scope);
+  // See TClingClassInfo::IsLoaded
+  bool IsComplete(TCppScope_t scope);
+
+  size_t SizeOf(TCppScope_t scope);
+  bool IsBuiltin(TCppType_t type);
+
+  bool IsTemplate(TCppScope_t handle);
+
+  bool IsAbstract(TCppType_t klass);
+
+  bool IsEnum(TCppScope_t handle);
+
+  bool IsVariable(TCppScope_t scope);
+
+  std::string GetName(TCppType_t klass);
+
+  std::vector<TCppScope_t> GetUsingNamespaces(TCppScope_t scope);
+
+} // end namespace InterOp
+
+} // end namespace cling
+
+#endif // CLING_INTERPRETER_INTEROP_H

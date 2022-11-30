eb72d720190c4272de1a954e6cb723b991a196e1
Baidyanath Kundu <kundubaidya99@gmail.com>
2022-11-30 16:44:50 +0100

    Implement InstantiateClassTemplate

--- include/cling/Interpreter/InterOp.h
+++ include/cling/Interpreter/InterOp.h
@@ -144,6 +144,8 @@ namespace InterOp {
   const std::string LookupLibrary(TInterp_t interp, const char *lib_name);
 
   bool LoadLibrary(TInterp_t interp, const char *lib_path);
+
+  TCppScope_t InstantiateClassTemplate(TInterp_t interp, const char *tmpl_name);
 } // end namespace InterOp
 
 } // end namespace cling
